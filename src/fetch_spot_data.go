62e351f6146e48e1c9c380acc86a33376b512b46
fjcloud <fj@msl.cloud>
2024-08-09 16:29:34 +0200

    refactor

--- src/fetch_spot_data.go
+++ src/fetch_spot_data.go
@@ -6,155 +6,192 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
-// Instance represents an EC2 instance type and its pricing details
 type Instance struct {
-	InstanceType   string  `json:"InstanceType"`
-	VCPUS          int     `json:"VCPUS"`
-	Memory         string  `json:"Memory"`
-	SpotSavingRate string  `json:"SpotSavingRate"`
-	SpotPrice      string  `json:"SpotPrice"`
-	PricePerVCPU   float64 `json:"PricePerVCPU"`
+	InstanceType   string `json:"InstanceType"`
+	VCPUS          int    `json:"VCPUS"`
+	Memory         string `json:"Memory"`
+	SpotSavingRate string `json:"SpotSavingRate"`
+	SpotPrice      string `json:"SpotPrice"`
 }
 
-// SpotData represents the entire dataset including all regions and global top 5 deals
+type Response struct {
+	Prices []Instance `json:"Prices"`
+}
+
+type Region struct {
+	Name      string `json:"name"`
+	Code      string `json:"code"`
+	Type      string `json:"type"`
+	Label     string `json:"label"`
+	Continent string `json:"continent"`
+}
+
+type GlobalDeal struct {
+	InstanceType string  `json:"instanceType"`
+	VCPUS        int     `json:"cpus"`
+	Memory       string  `json:"memory"`
+	SpotPrice    float64 `json:"price"`
+	PricePerVCPU float64 `json:"pricePerVCPU"`
+	Region       string  `json:"region"`
+}
+
 type SpotData struct {
 	LastUpdated string                `json:"last_updated"`
 	Regions     map[string][]Instance `json:"regions"`
-	GlobalTop5  []Instance            `json:"global_top_5"`
+	GlobalTop5  []GlobalDeal          `json:"global_top_5"`
 }
 
 func main() {
-	// Fetch and process the spot instance data
-	newData := fetchSpotData()
+	spotData := fetchSpotData()
 
-	// Read the existing data file, if it exists
-	oldData, _ := readExistingData("docs/spot_data.json")
-
-	// Compare old and new data, update file if there are changes
-	if dataChanged(oldData, newData) {
-		writeDataToFile(newData, "docs/spot_data.json")
-		log.Println("Data updated and saved.")
-	} else {
-		log.Println("No changes in data.")
+	file, err := os.Create("spot_data.json")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(spotData); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Data updated and saved.")
 }
 
-// fetchSpotData retrieves spot instance data for all regions
 func fetchSpotData() SpotData {
-	regions := fetchRegions()
+	regions, err := fetchRegions()
+	if err != nil {
+		log.Fatalf("Error fetching regions: %v", err)
+	}
+
+	var wg sync.WaitGroup
 	spotData := SpotData{
 		LastUpdated: time.Now().UTC().Format(time.RFC3339),
 		Regions:     make(map[string][]Instance),
 	}
+	var globalDeals []GlobalDeal
+	var mu sync.Mutex
 
-	var allInstances []Instance
-
-	// Fetch data for each region
 	for _, region := range regions {
-		instances := getSpotDeals(region)
-		spotData.Regions[region] = instances
-		allInstances = append(allInstances, instances...)
+		wg.Add(1)
+		go func(r string) {
+			defer wg.Done()
+			deals, err := getSpotDeals(r)
+			if err != nil {
+				log.Printf("Error getting spot deals for region %s: %v", r, err)
+				return
+			}
+			mu.Lock()
+			spotData.Regions[r] = deals
+			if len(deals) > 0 {
+				price, _ := strconv.ParseFloat(deals[0].SpotPrice, 64)
+				pricePerVCPU := price / float64(deals[0].VCPUS)
+				globalDeals = append(globalDeals, GlobalDeal{
+					InstanceType: deals[0].InstanceType,
+					VCPUS:        deals[0].VCPUS,
+					Memory:       deals[0].Memory,
+					SpotPrice:    price,
+					PricePerVCPU: pricePerVCPU,
+					Region:       r,
+				})
+			}
+			mu.Unlock()
+		}(region)
 	}
 
-	// Sort all instances by PricePerVCPU
-	sort.Slice(allInstances, func(i, j int) bool {
-		return allInstances[i].PricePerVCPU < allInstances[j].PricePerVCPU
+	wg.Wait()
+
+	sort.Slice(globalDeals, func(i, j int) bool {
+		return globalDeals[i].PricePerVCPU < globalDeals[j].PricePerVCPU
 	})
 
-	// Get top 5 global deals
-	if len(allInstances) > 5 {
-		spotData.GlobalTop5 = allInstances[:5]
+	if len(globalDeals) > 5 {
+		spotData.GlobalTop5 = globalDeals[:5]
 	} else {
-		spotData.GlobalTop5 = allInstances
+		spotData.GlobalTop5 = globalDeals
 	}
 
 	return spotData
 }
 
-// getSpotDeals fetches and processes spot deals for a specific region
-func getSpotDeals(region string) []Instance {
+func getSpotDeals(region string) ([]Instance, error) {
 	url := fmt.Sprintf("https://ec2.shop?region=%s&filter=ebs,cpu>=4,cpu<=32", region)
-	resp, err := http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Printf("Error fetching data for region %s: %v", region, err)
-		return nil
+		return nil, err
+	}
+	req.Header.Set("accept", "json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	var response struct {
-		Prices []Instance `json:"Prices"`
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
 	}
-	json.Unmarshal(body, &response)
+
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
 
 	var highSavingsInstances []Instance
 	for _, instance := range response.Prices {
-		savingsRate, _ := strconv.Atoi(strings.TrimSuffix(instance.SpotSavingRate, "%"))
-		if savingsRate > 50 {
-			price, _ := strconv.ParseFloat(instance.SpotPrice, 64)
-			instance.PricePerVCPU = price / float64(instance.VCPUS)
+		savingsRate, err := strconv.Atoi(strings.TrimSuffix(instance.SpotSavingRate, "%"))
+		if err == nil && savingsRate > 50 {
 			highSavingsInstances = append(highSavingsInstances, instance)
 		}
 	}
 
-	// Sort instances by PricePerVCPU
 	sort.Slice(highSavingsInstances, func(i, j int) bool {
-		return highSavingsInstances[i].PricePerVCPU < highSavingsInstances[j].PricePerVCPU
+		priceI, _ := strconv.ParseFloat(highSavingsInstances[i].SpotPrice, 64)
+		priceJ, _ := strconv.ParseFloat(highSavingsInstances[j].SpotPrice, 64)
+		ratioI := priceI / float64(highSavingsInstances[i].VCPUS)
+		ratioJ := priceJ / float64(highSavingsInstances[j].VCPUS)
+		return ratioI < ratioJ
 	})
 
-	return highSavingsInstances
+	return highSavingsInstances, nil
 }
 
-// fetchRegions retrieves the list of AWS regions
-func fetchRegions() []string {
+func fetchRegions() ([]string, error) {
 	resp, err := http.Get("https://b0.p.awsstatic.com/locations/1.0/aws/current/locations.json")
 	if err != nil {
-		log.Fatalf("Error fetching regions: %v", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	var regions map[string]struct {
-		Type string `json:"type"`
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
 	}
-	json.NewDecoder(resp.Body).Decode(&regions)
+
+	var regions map[string]Region
+	err = json.Unmarshal(body, &regions)
+	if err != nil {
+		return nil, err
+	}
 
 	var regionCodes []string
-	for code, info := range regions {
-		if info.Type == "AWS Region" {
-			regionCodes = append(regionCodes, code)
+	for _, region := range regions {
+		if region.Type == "AWS Region" {
+			regionCodes = append(regionCodes, region.Code)
 		}
 	}
 
 	sort.Strings(regionCodes)
-	return regionCodes
-}
-
-// readExistingData reads the existing spot data file
-func readExistingData(filename string) (SpotData, error) {
-	var data SpotData
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return data, err
-	}
-	err = json.Unmarshal(file, &data)
-	return data, err
-}
-
-// writeDataToFile writes the spot data to a JSON file
-func writeDataToFile(data SpotData, filename string) {
-	file, _ := json.MarshalIndent(data, "", "  ")
-	ioutil.WriteFile(filename, file, 0644)
-}
-
-// dataChanged checks if there are any differences between old and new data
-func dataChanged(old, new SpotData) bool {
-	oldData, _ := json.Marshal(old)
-	newData, _ := json.Marshal(new)
-	return string(oldData) != string(newData)
+	return regionCodes, nil
 }
